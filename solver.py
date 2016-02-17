1c11cd484186ba38266b66ed893bc22d2d982622
Aaron Flanagan <aaronflanagan044@hotmail.com>
2016-02-17 17:50:35 +0000

    parser, wordlist and solver created

---
+++ solver.py
@@ -0,0 +1,20 @@
+# Aaron Flanagan
+# G00330035
+# 17/03/2016
+
+# Import random to shuffle a list.
+import random as rn
+
+# This preprocessing function loads the words list file into a Python list.
+def preprocessing(filename="wordlist.txt"):
+  return []
+  
+# This is the function that actually checks the random letters for words.
+def check(letters):
+  while (letters):
+    letters.pop()
+  return []
+
+# This function is just a wrapper that shows how my script works.
+# It does the preprocessing, then creates a random list of letters, and finally runs the solver.
+Raw
